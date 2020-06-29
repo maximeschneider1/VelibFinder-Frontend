cec253860f4d76ef2ba1aac3e1958a2d0cd2d60a
Maxime Schneider <m_schneider@etu-webschoolfactory.fr>
2020-06-29 16:00:38 +0200

    added mechanical and electric view

--- src/Components/Station.js
+++ src/Components/Station.js
@@ -6,7 +6,6 @@ import CardContent from "@material-ui/core/CardContent";
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-
     textAlign: "left",
   },
   stationWrapper: {
@@ -14,26 +13,44 @@ const useStyles = makeStyles({
     overflow: "auto",
     margin: "2%",
   },
+  velibsTotalWrapper: {
+    display: "flex",
+  },
+  velibTotalText: {
+    marginLeft: '10px'
+  }
 });
 
 export default function Station({ station }) {
   const classes = useStyles();
 
-
   return (
     <div className={classes.stationWrapper}>
       <Card className={classes.root}>
-      <img
-                src={station.station_img}
-                width="300px"
-                height="175px"
-                alt=""
-              />
+        <img src={station.station_img} width="300px" height="175px" alt="" />
         <CardContent>
           <div>
             <div>
               <h3>{station.station}</h3>
-              <h4>Vélib' disponibles : {station.num_bikes_available}</h4>
+              <h4>Vélib disponibles : {station.num_bikes_available}</h4>
+              <div className={classes.velibsTotalWrapper}>
+                <img
+                  src="https://www.velib-metropole.fr/assets/static_files/images/velo_meca_green@2x.png"
+                  alt="mechanical-velib"
+                  width='50px'
+                  height='30px'
+                />
+                <h4 className={classes.velibTotalText}>{station.num_mechanical_bikes} Vélib méchaniques</h4>
+              </div>
+              <div className={classes.velibsTotalWrapper}>
+                <img
+                  src="https://www.velib-metropole.fr/assets/static_files/images/velo_elec_bleu@2x.png"
+                  alt="electrical-velib"
+                  height='50px'
+                  width='50px'
+                />
+                <h4 className={classes.velibTotalText}>{station.num_electric_bikes} Vélib électriques </h4>
+              </div>
             </div>
           </div>
         </CardContent>
