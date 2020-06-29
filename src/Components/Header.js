0bc750257f7bc2b475eac2aa5edda450eb03c7bc
Maxime Schneider <m_schneider@etu-webschoolfactory.fr>
2020-06-29 16:42:13 +0200

    css cleanup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,29 @@
 import React  from "react";
 import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end'
+  },
+  h1: {
+    marginLeft: '5%',
+    marginRight: '5%',
+    marginBottom: '2%'
+  }
+});
 
 export default function Header() {
+  const classes = useStyles();
 
   return (
     <React.Fragment>
       <AppBar position="static" color="primary">
-            <div className="toolbar">
-                <h1>Splio Velib' Finder</h1>
-                <h3>Maxime Schneider</h3>
+            <div className={classes.toolbar}>
+                <h1 className={classes.h1}>Splio Velib' Finder</h1>
+                <h3 className={classes.h1}>Maxime Schneider</h3>
             </div>
       </AppBar>
     </React.Fragment>
